937841416433bb9df7d242c043972bcee40ff5d9
Adarsh01208 <adarshsingh01209@gmail.com>
2025-01-30 20:33:09 +0530

    refactor: clean up unused imports and improve component structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router'; 
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ViewUsersComponent } from './view-users/view-users.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , SidebarComponent, NavbarComponent , ViewUsersComponent],
+  imports: [RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
